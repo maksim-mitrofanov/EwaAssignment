20977d23476a1060c4af990411bbdf52094be78d
Maksim Mitrofanov <maximumthingstodo@gmail.com>
2024-04-02 19:24:04 +0400

    Initial Commit

---
+++ EwaAssignment/EwaAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EwaAssignmentApp.swift
+//  EwaAssignment
+//
+//  Created by  Maksim on 02.04.24.
+//
+
+import SwiftUI
+
+@main
+struct EwaAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
